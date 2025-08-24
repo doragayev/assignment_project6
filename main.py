0137cb0da2e6838cb02acab936f057818740c139
doragayev <137411955+doragayev@users.noreply.github.com>
2025-08-24 18:37:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import datetime
 
 print("Welcome to Git Assignment")
 print("Current date and time:", datetime.datetime.now())
-print("need new change for make a conflict")
 print("My name is Dor Agayev")
 
